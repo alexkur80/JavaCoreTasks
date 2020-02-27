51ae54a9e4268de79bccf5d73873b266f2a4486d
Alexander K <60927144+alexkur80@users.noreply.github.com>
2020-02-27 14:24:51 +0300

    lection3

--- src/com/myproject/Utils.java
+++ src/com/myproject/Utils.java
@@ -2,6 +2,9 @@
  * @author  Kurlovich Alexander
  * @param int checkScannerIntNumber (Scanner sc1, int intNumber)
  *            Request Int number from console and check if it is Int or not.
+ *
+ * @param int checkScannerIntNumberNew(Scanner sc1, String invitation)
+ *             request String invitation. Verify if input  amount <0  and request again.
  * @return    intNumber  - requested number from console.
  */
 
@@ -12,54 +15,33 @@ import java.util.Scanner;
 
 public class Utils {
     public static void main(String[] args) {
-
     }
 
 
-    public static int checkScannerIntNumber (Scanner sc1){
+    public static int checkScannerIntNumber(Scanner sc1) {
         int intNumber = 0;
         System.out.print("Input int number but NOT ZERO: ");
         if (sc1.hasNextInt()) {
             intNumber = sc1.nextInt();
 
-        }  else {
+        } else {
             System.out.println("You input wrong number, try again");
 
         }
         return intNumber;
     }
 
-
-
-
-
-/*    public static int checkScannerIntNumberNew (Scanner sc1, String invitation){
+    public static int checkScannerIntNumberNew(Scanner sc1, String invitation) {
         int intNumber = 0;
 
-        System.out.print(invitation);
-        if (sc1.hasNextInt()) {
+        do {
+            System.out.print(invitation);
             intNumber = sc1.nextInt();
-
-        }  else {
-            System.out.println("You input wrong number, try again");
         }
-        return intNumber;
-    }*/
-
-
-    public static int checkScannerIntNumberNew (Scanner sc1, String invitation){
-        int intNumber = 0;
-
-        System.out.print(invitation);
-        if (sc1.hasNextInt()) {
-            intNumber = sc1.nextInt();
-
-        }  else {
-            System.out.println("You input wrong number, try again");
-        }
+        while (intNumber <= 0);
         return intNumber;
     }
-
 
 
 }
+
