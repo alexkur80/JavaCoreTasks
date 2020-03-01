b1e6d470daf5b7db7571a887a1aa2404685152b8
Alexander K <60927144+alexkur80@users.noreply.github.com>
2020-03-01 16:13:12 +0300

    ATM is full, check just for % not checked for banknotes

--- src/com/myproject/Lection04/Lection04Task02.java
+++ src/com/myproject/Lection04/Lection04Task02.java
@@ -1,3 +1,14 @@
+/**
+ * This class modulate ATM system. Banknotes have three parameters:
+ * banknotes20Usd, banknotes50Usd, banknotes100Usd defined in Constructor.
+ *
+ * @author  Kurlovich Alexander
+ * @version Lection04Task02
+ * @param int Atm pushToAtm() - method that accept funds in ATM.
+ * @param boolean pullOutOfAtm(Atm atmMachine) - return boolean of success procedure.
+ *
+ */
+
 package com.myproject.Lection04;
 
 import java.util.Scanner;
@@ -9,7 +20,6 @@ class Atm {
     int banknotes50Usd;
     int banknotes100Usd;
 
-
     Atm(int usd20, int usd50, int usd100) {
         banknotes20Usd = usd20;
         banknotes50Usd = usd50;
@@ -19,47 +29,50 @@ class Atm {
 
     static Atm pushToAtm() {
         Scanner sc = new Scanner(System.in);
-        int usd20 = checkScannerIntNumberNew(sc, "Input amount of 20$ banknotes: ");
-        int usd50 = checkScannerIntNumberNew(sc, "Input amount of 50$ banknotes: ");
-        int usd100 = checkScannerIntNumberNew(sc, "Input amount of 100$ banknotes: ");
+        int usd20 = checkScannerIntNumberNew(sc, "Input amount of 20$ banknotes:\t");
+        int usd50 = checkScannerIntNumberNew(sc, "Input amount of 50$ banknotes:\t");
+        int usd100 = checkScannerIntNumberNew(sc, "Input amount of 100$ banknotes:\t");
         Atm atmMachine = new Atm(usd20, usd50, usd100);
         return atmMachine;
     }
 
 
-    static boolean pullOutOfAtm(Atm atmMachine) {
-        boolean flag;
+    static boolean WithdrawOutOfAtm(Atm atmMachine) {
+        boolean flag = false;
         Scanner sc = new Scanner(System.in);
-        int cashToOutNow = checkScannerIntNumberNew(sc, "How much USD you want to pull out: ");
-        int allCashInAtmToday = atmMachine.banknotes20Usd * 20 + atmMachine.banknotes50Usd * 50 +
+        System.out.println("\nWe have banknotes only 100$, 50$, 20$  in ATM");
+
+// Check if ATM can send funds - if cashToOutNow % ANY BANKNOTE == 0
+        int cashToWithdrawNow = checkScannerIntNumberNew(sc, "How much USD you want to withdraw: ");
+        if (cashToWithdrawNow % 100 == 0 || cashToWithdrawNow % 50 == 0 || cashToWithdrawNow % 20 == 0) {
+            flag = true;
+        } else {
+            return false;
+        }
+
+        int allCashInAtm = atmMachine.banknotes20Usd * 20 + atmMachine.banknotes50Usd * 50 +
                 atmMachine.banknotes100Usd * 100;
-        if (cashToOutNow > allCashInAtmToday) {
-            System.out.println("We have just " + allCashInAtmToday + " USD in ATM and not able to give you amount" +
-                    "you need.");
+        if (cashToWithdrawNow > allCashInAtm) {
+            System.out.println("You want to withdraw " + cashToWithdrawNow + " USD but we have just " + allCashInAtm +
+                    " USD in ATM, find another ATM");
             flag = false;
-        } else flag = true;
-
+            return flag;
+        }
         return flag;
-
     }
-
 }
 
 
 public class Lection04Task02 {
     public static void main(String[] args) {
 
-        // Put in atmMachine link to object that we just filled with $$$$$
+// We need class atmMachine for future method so it doesn't should be lost!
         Atm atmMachine = Atm.pushToAtm();
 
-        // Return Flag of success ot false pullOutOfAtm operation.
-
-        boolean flagSuccess = Atm.pullOutOfAtm (atmMachine);
-
-        System.out.println(flagSuccess);
-
+// Return Flag of pullOutOfAtm operation.
+        boolean flag = Atm.WithdrawOutOfAtm(atmMachine);
+        System.out.println("Withdrawal result = " + flag);
     }
-
 }
 
 
