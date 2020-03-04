ca3b2e990ff428c1e87a320680a8fa0cf162b2ac
Alexander K <60927144+alexkur80@users.noreply.github.com>
2020-03-04 17:09:50 +0300

    Overloading methods and constructors

--- src/com/myproject/lection05/Lection05Task01a.java
+++ src/com/myproject/lection05/Lection05Task01a.java
@@ -1,7 +1,7 @@
 /**
- * This class accepts two INT, DOUBLE. Initialize class object by two numbers (it should have 3 Constructors).
- * Make Overloading of constructors and create method that return result of math operation.
- * In this code returns A + B
+ * Make class and create Overloading methods. Call for methods that calculate and return sum of two fields.
+ * Make sum of: two int numbers, two double numbers, two BigDecimal numbers.
+ *
  *
  * @author Kurlovich Alexander
  * @version Lection04Task02
@@ -18,45 +18,23 @@ package com.myproject.lection05;
 
 import java.math.BigDecimal;
 
-class OverloadingOfMethods2 {
-    int intNumberA;
-    int intNumberB;
-    double doubleNumberA;
-    double doubleNumberB;
-    BigDecimal bigDecimalNumberA;
-    BigDecimal bigDecimalNumberB;
+class OverloadingOfMethods {
 
-    OverloadingOfMethods2(int intNumberA, int intNumberB) {
-        this.intNumberA = intNumberA;
-        this.intNumberB = intNumberB;
-    }
-
-    OverloadingOfMethods2(double doubleNumberA, double doubleNumberB) {
-        this.doubleNumberA = doubleNumberA;
-        this.doubleNumberB = doubleNumberB;
-    }
-
-    OverloadingOfMethods2(BigDecimal bigDecimalNumberA, BigDecimal bigDecimalNumberB) {
-        this.bigDecimalNumberA = bigDecimalNumberA;
-        this.bigDecimalNumberB = bigDecimalNumberB;
-    }
-
-
-    int intMethod() {
+    int summOverloading(int intNumberA, int intNumberB) {
         int result = 0;
         result = intNumberA + intNumberB;
 
         return result;
     }
 
-    double doubleMethod() {
+    double summOverloading(double doubleNumberA, double doubleNumberB) {
         double result = 0;
         result = doubleNumberA + doubleNumberB;
 
         return result;
     }
 
-    BigDecimal bigDecimalMethod() {
+    BigDecimal summOverloading(BigDecimal bigDecimalNumberA, BigDecimal bigDecimalNumberB) {
         BigDecimal result = null;
         result = bigDecimalNumberA.add(bigDecimalNumberB);
 
@@ -65,7 +43,7 @@ class OverloadingOfMethods2 {
     }
 }
 
-public class Lection05Task01 {
+public class Lection05Task01a {
     public static void main(String[] args) {
 
         int intNumberA = 50;
@@ -80,19 +58,17 @@ public class Lection05Task01 {
         BigDecimal bigDecimalNumberA = new BigDecimal("1111111111111111.12312312");
         BigDecimal bigDecimalNumberB = new BigDecimal("2222222222222222.12323243");
 
-        OverloadingOfMethods2 intOverloadingObj = new OverloadingOfMethods2(intNumberA, intNumberB);
-        OverloadingOfMethods2 doubleOverloadingObj = new OverloadingOfMethods2(doubleNumberA, doubleNumberB);
-        OverloadingOfMethods2 bigDecimalOverloadingObj = new OverloadingOfMethods2(bigDecimalNumberA, bigDecimalNumberB);
+        OverloadingOfMethods overloadingObj = new OverloadingOfMethods();
 
-        resultIntNumbers = intOverloadingObj.intMethod();
+        resultIntNumbers = overloadingObj.summOverloading(intNumberA, intNumberB);
         System.out.println("Sum of " + intNumberA + " + " + intNumberB + " = "
                 + resultIntNumbers);
 
-        resultDoubleNumbers = doubleOverloadingObj.doubleMethod();
+        resultDoubleNumbers = overloadingObj.summOverloading(doubleNumberA, doubleNumberB);
         System.out.println("Sum of " + doubleNumberA + " + " + doubleNumberB + " = "
                 + resultDoubleNumbers);
 
-        resultBigDecimalNumbers = bigDecimalOverloadingObj.bigDecimalMethod();
+        resultBigDecimalNumbers = overloadingObj.summOverloading(bigDecimalNumberA, bigDecimalNumberB);
         System.out.println("Sum of " + bigDecimalNumberA + " + " + bigDecimalNumberB + " = "
                 + resultBigDecimalNumbers);
 
