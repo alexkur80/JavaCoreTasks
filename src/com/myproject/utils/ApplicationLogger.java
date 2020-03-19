c2528d459f6748e8736f9d46ac04cf2041c54b44
Alexander K <60927144+alexkur80@users.noreply.github.com>
2020-03-19 12:10:00 +0300

    Log4j util. Finished

---
+++ src/com/myproject/utils/ApplicationLogger.java
@@ -0,0 +1,8 @@
+package com.myproject.utils;
+
+import org.apache.log4j.Logger;
+
+public class ApplicationLogger {
+    public final static Logger LOGGER = Logger.getLogger(ApplicationLogger.class);
+
+}
