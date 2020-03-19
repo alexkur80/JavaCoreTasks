d3c4fa26c5737947cab47b14d8efe9b63426df99
Alexander K <60927144+alexkur80@users.noreply.github.com>
2020-03-19 11:47:15 +0300

    RegExpTesting. FINISHED

--- src/com/myproject/lection14/RegExpTesting.java
+++ src/com/myproject/lection14/RegExpTesting.java
@@ -10,21 +10,24 @@
 package com.myproject.lection14;
 
 import com.myproject.utils.ApplicationLogger;
+import jdk.jfr.SettingControl;
 
 public class RegExpTesting {
 
     public static void main(String[] args) {
-        String emailToValidate = "sdfsdf @aol.com abababab ab sdfsdfsd.com"
-                + "sfsdf sdf s Lots of punctuation sadfasdf_sdfsd@aol.com.erf"
-                + "asdfsdf edsasds@sd.cm.efdd as________21323123__sdfsdfdas df"
-                + "as___3123__sdfsdf@dasdf ____21323123__sdfsdf@eu.com.org.gov";
+        String emailToValidate =
+                "bill@aol.com 22bill@aol.com _bill@aol.com " +
+                        "sfsdf sdf s Lots of punctuation sadfasdf_sdfsd@aol.com.erf" +
+                        "asdfsdf edsasds@sd.cm.efdd.sd.sdsa" +
+                        "_sdfsdf@eu.com.org.gov" +
+                        "pupkin@ao.com1.com.org";
         String hexToValidate =
                 "0x00adas 0x11321 0x12 " +
                         "0x13 0x18 19asdas0x2ffffwdasd " +
                         "asdf0x1a asdf0x1bdfsa 0x1c 0x1d " +
                         "1e0x 0x1f280x29 0x2a 0x2b 0x2c 0x2dh" +
                         " 0x2edasd 0x2fasdfasdf sad";
-        String tegsToReplace =
+        String tegToReplace =
                 "<blockquote><font color=\"#66cc66\">&gt;&gt;&gt;</font>&nbsp" +
                         ";<font color=\"#ff7700\">import</font>&nbsp;<font color" +
                         "=\"#dc143c\">re</font>   <br>\n" +
@@ -36,6 +39,16 @@ public class RegExpTesting {
                         "=\"#ff7700\">print</font>&nbsp;p<br>";
         String replacementTemplate = "<p>";
 
+        String phoneToValidate =
+                "+375123424525+37512312343322+3751234223323375123123433\n" +
+                        "+3751222345232+375126782344212312312" +
+                        "+375123442333434341234+375123242" +
+                        "+3751222342332ddsa231212 +3752950808102" +
+                        "+375122242234678+37512678234423123123";
+
+        String ipv4ToValidate = "0.0.0.0  0.0.1.0 255.0.0.1 255.55.255.255 " +
+                "192.168.0.1  001.0.0.0 256.1.1.1 1.1.1.1. \n" +
+                "-1.0.-1.1 ";
 
         String resultRegExp;
 
@@ -45,9 +58,14 @@ public class RegExpTesting {
         resultRegExp = UtilsRegExp.hexValidator(hexToValidate);
         ApplicationLogger.LOGGER.info(resultRegExp);
 
-        resultRegExp = UtilsRegExp.tegReplacement(tegsToReplace, replacementTemplate);
+        resultRegExp = UtilsRegExp.tegReplacement(tegToReplace, replacementTemplate);
         ApplicationLogger.LOGGER.info(resultRegExp);
 
+        resultRegExp = UtilsRegExp.phoneValidator(phoneToValidate);
+        ApplicationLogger.LOGGER.info(resultRegExp);
+
+        resultRegExp = UtilsRegExp.ipV4Validator(ipv4ToValidate);
+        ApplicationLogger.LOGGER.info(resultRegExp);
     }
 
 }
--- src/com/myproject/lection14/UtilsRegExp.java
+++ src/com/myproject/lection14/UtilsRegExp.java
@@ -1,3 +1,11 @@
+/**
+ * This class include RegExt Methods
+ *
+ * @author Kurlovich Alexander
+ * @version Lection14 RegExp
+ * @date 03/18/2020
+ */
+
 package com.myproject.lection14;
 
 import com.myproject.utils.ApplicationLogger;
@@ -7,25 +15,32 @@ import java.util.regex.Pattern;
 
 public class UtilsRegExp {
 
+
     /**
      * This class uses RegEx to validate email addresses from raw text. Email template should have digits, letters,
      * sign '_' but not start with '_'. It should have domen upper level org.com.by
      * It have restriction on domen length    - from 1 to 7 symbols/
      * Domen doesn't should end by '_'
-     * RegExp   [^\W_0-9]\w+@\w+(\.[^W_ .]{3,5})+
+     * RegExp   (?<=\s|^)([^\W_0-9]\w+)[@]([^\W_]+\.)+[^\W_]{2,7}(?=\s|$)
      * Description:
-     * [^\W_0-9]    Start any letter and number exclude '_' and digits from '0-9"
-     * \w+@\w+  any symbol and digits from one to more, then '@' and then again symbol and digits from one to more
-     * (\.[^W_ .]{1,7})+    group. Start any symbol, then any letter and digits, exclude '_', "Space", '.'
+     * (?<=\s|^)  - positive look behind  - if starts with '\s' | '^' then we are looking next condition
+     * ([^\W_0-9]\w+) - group 1, verify that starts from letter only, exclude '_' and 0-9'
+     * and then any letter 'w\+' include digits and letters
+     * [@] - then'@' every email has
+     * ([^\W_]+\.)+    this group include letters, digits exclude '_' from one to many times and then
+     * include '.'
+     * [^\W_]{2,7}(?=\s) - this verify if include domen upper level inclide all letters and digits,
+     * exclude '_' and have length {2,7}
+     * (?=\s|$)   positive look ahead - see, if next symbol finishes with '\s' or this is last symbol of text
+     * <p>
      * from 1 to 7 and repeat from one to more. It makes using higher level domens.
      *
-     * @param rawTextToGetEmails raw text where to find correct emails;
+     * @param emailToValidate raw text where to find correct emails;
      */
-    public static String emailValidator(String rawTextToGetEmails) {
+    public static String emailValidator(String emailToValidate) {
 
-        Pattern pattern = Pattern.compile("[^\\W_0-9]\\w+@\\w+(\\.[^W_ .]{3,5})+", Pattern.MULTILINE);
-        Matcher matcher = pattern.matcher(rawTextToGetEmails);
-
+        Pattern pattern = Pattern.compile("(?<=\\s|^)([^\\W_0-9]\\w+)[@]([^\\W_]+\\.)+[^\\W_]{2,7}(?=\\s|$)", Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(emailToValidate);
         StringBuilder stringbuilder = new StringBuilder();
 
         while (matcher.find()) {
@@ -34,11 +49,12 @@ public class UtilsRegExp {
                     .append(" ");
         }
 
-        ApplicationLogger.LOGGER.info("Before: " + rawTextToGetEmails);
+        ApplicationLogger.LOGGER.info("Before: " + emailToValidate);
         String result = stringbuilder.toString();
         return "After " + result;
 
     }
+
 
     /**
      * This class uses RegEx to find valid HEX numbers and get them.
@@ -48,13 +64,12 @@ public class UtilsRegExp {
      * ([0-9]|[a-fA-F])+   group. Here we are looking for digits from ;0-9' or letters from 'A-F, a-f"
      * that use fox HEX. Group can be long because of big HEX number
      *
-     * @param rawTextToGetHex raw text where to find correct HEX numbers;
+     * @param hexToValidate raw text where to find correct HEX numbers;
      */
-    public static String hexValidator(String rawTextToGetHex) {
+    public static String hexValidator(String hexToValidate) {
 
         Pattern pattern = Pattern.compile("0x([0-9]|[a-fA-F])+", Pattern.MULTILINE);
-        Matcher matcher = pattern.matcher(rawTextToGetHex);
-
+        Matcher matcher = pattern.matcher(hexToValidate);
         StringBuilder stringbuilder = new StringBuilder();
 
         while (matcher.find()) {
@@ -62,7 +77,7 @@ public class UtilsRegExp {
                     .append(" ");
         }
 
-        ApplicationLogger.LOGGER.info("Before:" + rawTextToGetHex);
+        ApplicationLogger.LOGGER.info("Before:" + hexToValidate);
         String result = stringbuilder.toString();
         return "After " + result;
 
@@ -71,7 +86,7 @@ public class UtilsRegExp {
 
     /**
      * This class uses RegEx to find symbols between tegs <qwqwe 2SDASD> and replace
-     * for <p>
+     * by <p>
      * RegExp   <(.|\n)*?>
      * Description
      * < start with '<'
@@ -79,18 +94,111 @@ public class UtilsRegExp {
      * and repeat it many times or not and '?' means preveous action is not nessesarry.
      * Finish with > teg
      *
-     * @param rawTextToReplaceTegs
-     * @param replacementString
+     * @param tegToReplace
+     * @param replacementTemplate
      */
-    public static String tegReplacement(String rawTextToReplaceTegs, String replacementString) {
+    public static String tegReplacement(String tegToReplace, String replacementTemplate) {
 
         Pattern pattern = Pattern.compile("<(.|\\n)*?>", Pattern.MULTILINE);
-        Matcher matcher = pattern.matcher(rawTextToReplaceTegs);
+        Matcher matcher = pattern.matcher(tegToReplace);
 
-        ApplicationLogger.LOGGER.info("Before: " + rawTextToReplaceTegs);
+        ApplicationLogger.LOGGER.info("Before: " + tegToReplace);
 
-        String result = matcher.replaceAll(replacementString).toString();
+        String result = matcher.replaceAll(replacementTemplate);
         return "After " + result;
+
+    }
+
+
+    /**
+     * This class uses RegEx to find phoneNumbers from format +375000000000 and output to correct format
+     * +375 (XX) YYY-YY-YY
+     * RegEx    (\+375)(\d{2})(\d{3})(\d{2})(\d{2})
+     * important: Since we need to output in some format - regex should be grouped to collect
+     * easy using StringBuilder append method/
+     * (\+375)  group start '+375' must start every phone number
+     * \d{2})(\d{3})(\d{2})(\d{2})  four other groups devided number for group for 2, 3,2,2 digits
+     * and after matcher finishes - every group keeps in matcher.group(n) so it it easy to
+     * extract every group in while cycle and collect phone numbers in correct format.
+     *
+     * @param phoneNumToValidate raw text containing phone numbers
+     * @return phone numbers in format +375 (XX) YYY-YY-YY
+     */
+    public static String phoneValidator(String phoneNumToValidate) {
+
+        Pattern pattern = Pattern.compile("(\\+375)(\\d{2})(\\d{3})(\\d{2})(\\d{2})", Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(phoneNumToValidate);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (matcher.find()) {
+
+            stringBuilder
+                    .append(" ")
+                    .append(matcher.group(1))
+                    .append("(")
+                    .append(matcher.group(2))
+                    .append(")")
+                    .append(matcher.group(3))
+                    .append("-")
+                    .append(matcher.group(4))
+                    .append("-")
+                    .append(matcher.group(5));
+        }
+
+        ApplicationLogger.LOGGER.info("Before:" + phoneNumToValidate);
+        String result = stringBuilder.toString();
+        return "After " + result;
+
+    }
+
+
+    /**
+     * This class uses RegEx to find correct ipv4ToValidate
+     * Example valid Ipv4:
+     * 0.0.0.0
+     * 0.0.1.0
+     * 255.0.0.1
+     * 255.55.255.255
+     * 192.168.0.1
+     * <p>
+     * Example not valid Ipv4
+     * 001.0.0.0
+     * 256.1.1.1
+     * 1.1.1.1.
+     * -1.0.-1.1
+     * <p>
+     * Regexp
+     * it has 4 equal group code so here is description.
+     * To cover every legit digits it shouldb following rules
+     * (25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)     - can start with '25' and be only max 255, so
+     * 25[0-5]  means it can be 250-255. OR it cab ne max 249, so 2[0-4][0-9] cover from 200 to 249
+     * [1]?[0-9][0-9]?  means [1]? can be 1 one time or not to be, then it must be [0-9] and [0-9]? means
+     * from 0 to 9 can be one time or not to be.
+     * There is some important. First - IP should start from word boundary '\b' and finish only digits,
+     * so we use Positive look behind  (?=\s|$) -  means it finish with \s or at the end of line $
+     *
+     * @param ipv4ToValidate raw text containing phone numbers
+     * @return valid IPv4
+     */
+    public static String ipV4Validator(String ipv4ToValidate) {
+
+        Pattern pattern = Pattern.compile("\\b(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?=\\s|$)", Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(ipv4ToValidate);
+        StringBuilder stringbuilder = new StringBuilder();
+
+        while (matcher.find()) {
+
+            stringbuilder
+                    .append(" ")
+                    .append(matcher.group());
+        }
+
+        ApplicationLogger.LOGGER.info("Before: " + ipv4ToValidate);
+        String result = stringbuilder.toString();
+        return "After" + result;
+
     }
 
 }
+
+
