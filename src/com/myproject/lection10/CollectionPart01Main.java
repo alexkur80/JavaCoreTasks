04723de4298677169d99f5b11adb54e5561750b0
Alexander K <60927144+alexkur80@users.noreply.github.com>
2020-03-27 13:55:07 +0300

    Collections. Part I. FINISHED

--- src/com/myproject/lection10/CollectionPart01Main.java
+++ src/com/myproject/lection10/CollectionPart01Main.java
@@ -13,7 +13,7 @@ import com.myproject.utils.ApplicationLogger;
 import java.io.IOException;
 import java.util.*;
 
-public class CollectionMain {
+public class CollectionPart01Main {
     public static void main(String[] args) {
 
         Students.studentMarks();
