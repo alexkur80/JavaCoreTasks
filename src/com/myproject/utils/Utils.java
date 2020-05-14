8a4d90bf9f98254b725c2c75f3b9e7ccce687de0
Alexander K <60927144+alexkur80@users.noreply.github.com>
2020-05-14 19:10:04 +0400

    Utils

--- src/com/myproject/utils/Utils.java
+++ src/com/myproject/utils/Utils.java
@@ -1,26 +1,22 @@
-/**
- * @author  Kurlovich Alexander
- * @param int checkScannerIntNumber (Scanner sc1, int intNumber)
- *            Request Int number from console and check if it is Int or not.
- *
- * @param int checkScannerIntNumberNew(Scanner sc1, String invitation)
- *             request String invitation. Verify if input  amount <0  and request again.
- * @return    intNumber  - requested number from console.
- */
-
-
 package com.myproject.utils;
 
 import java.util.Scanner;
 
 public class Utils {
 
-
-    public static int checkScannerIntNumber(Scanner sc1) {
+    /**
+     * Accepts int number
+     *
+     * @return int number
+     */
+    public static int checkScannerIntNumber() {
         int intNumber = 0;
+        Scanner sc1 = new Scanner(System.in);
+
         System.out.print("Input int number but NOT ZERO: ");
         if (sc1.hasNextInt()) {
             intNumber = sc1.nextInt();
+
 
         } else {
             System.out.println("You input wrong number, try again");
@@ -29,22 +25,21 @@ public class Utils {
         return intNumber;
     }
 
+    /**
+     * Accepts input number > 0
+     *
+     * @param invitation String
+     * @return int entered number above ZERO
+     */
+    public static int checkScannerIntNumberNew(String invitation) {
+        Scanner sc = new Scanner(System.in);
 
-// Verify if input number > 0
-
-    public static int checkScannerIntNumberNew(Scanner sc1, String invitation) {
         int intNumber = 0;
-
         do {
             System.out.print(invitation);
-            intNumber = sc1.nextInt();
+            intNumber = sc.nextInt();
         }
         while (intNumber < 0);
         return intNumber;
     }
-
-
-
 }
-
-
